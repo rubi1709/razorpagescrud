7039092b47bb42ee98a431e49c944c13e586bbf1
Felicitas Rubi Diego Garcia <23300084@uttt.edu.mx>
2026-03-11 10:25:05 -0600

    Fix RazorPages service

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 using RazorPages.Data;
 using Microsoft.EntityFrameworkCore;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// AGREGAR RAZOR PAGES
+builder.Services.AddRazorPages();
+
+// CONEXIÓN A POSTGRESQL (Railway)
 var connection = Environment.GetEnvironmentVariable("DATABASE_URL");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -11,11 +14,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseRouting();
 
 app.MapRazorPages();
 
+// MIGRACIONES AUTOMÁTICAS
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
