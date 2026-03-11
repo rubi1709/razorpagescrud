5a0e8b9c83fbcca68f450c3fc30f179ef22799a6
Felicitas Rubi Diego Garcia <23300084@uttt.edu.mx>
2026-03-11 11:45:48 -0600

    fix: stable PostgreSQL connection for Railway deployment

--- Program.cs
+++ Program.cs
@@ -3,28 +3,39 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Razor Pages
 builder.Services.AddRazorPages();
 
-// Variables que Railway crea automáticamente al conectar Postgres
+// Leer variables de Railway
 var host = Environment.GetEnvironmentVariable("PGHOST");
 var port = Environment.GetEnvironmentVariable("PGPORT");
 var database = Environment.GetEnvironmentVariable("PGDATABASE");
 var username = Environment.GetEnvironmentVariable("PGUSER");
 var password = Environment.GetEnvironmentVariable("PGPASSWORD");
 
+// Crear cadena de conexión
 var connectionString =
-    $"Host={host};Port={port};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+    $"Host={host};" +
+    $"Port={port};" +
+    $"Database={database};" +
+    $"Username={username};" +
+    $"Password={password};" +
+    $"SSL Mode=Require;" +
+    $"Trust Server Certificate=true";
 
+// Registrar DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
+// Middleware
 app.UseStaticFiles();
 app.UseRouting();
 
 app.MapRazorPages();
 
+// Aplicar migraciones automáticamente
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
