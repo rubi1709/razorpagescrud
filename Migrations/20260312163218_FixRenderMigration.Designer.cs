1664a8641ba07da7e3d56eb6cf06d4bd5527e2d9
Felicitas Rubi Diego Garcia <23300084@uttt.edu.mx>
2026-03-12 10:38:08 -0600

    fix connection string syntax

--- Migrations/20260312163218_FixRenderMigration.Designer.cs
+++ Migrations/20260312163218_FixRenderMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RazorPages.Data;
@@ -11,9 +12,11 @@ using RazorPages.Data;
 namespace RazorPages.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260312163218_FixRenderMigration")]
+    partial class FixRenderMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260312163218_FixRenderMigration.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RazorPages.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRenderMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "Usuarios",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Usuarios",
+                type: "character varying(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(70)",
+                oldMaxLength: 70);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaRegistro",
+                table: "Usuarios",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Correo",
+                table: "Usuarios",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellido",
+                table: "Usuarios",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Usuarios",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "Usuarios",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Usuarios",
+                type: "nvarchar(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(70)",
+                oldMaxLength: 70);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaRegistro",
+                table: "Usuarios",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Correo",
+                table: "Usuarios",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellido",
+                table: "Usuarios",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Usuarios",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
