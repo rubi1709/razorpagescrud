51ba21a24043bf88d25023a364bb897e65208d55
Felicitas Rubi Diego Garcia <23300084@uttt.edu.mx>
2026-03-12 08:48:48 -0600

    program.cs production ready

--- Program.cs
+++ Program.cs
@@ -3,33 +3,47 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Razor Pages
 builder.Services.AddRazorPages();
 
+// Leer DATABASE_URL (Render) o usar DefaultConnection (local)
 var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+string connectionString;
 
 if (!string.IsNullOrEmpty(databaseUrl))
 {
+    // Convertir la URL de PostgreSQL a ConnectionString de Npgsql
     var uri = new Uri(databaseUrl);
     var userInfo = uri.UserInfo.Split(':');
 
-    var connectionString =
+    connectionString =
         $"Host={uri.Host};" +
         $"Port={uri.Port};" +
         $"Database={uri.AbsolutePath.TrimStart('/')};" +
         $"Username={userInfo[0]};" +
         $"Password={userInfo[1]};" +
         $"SSL Mode=Require;Trust Server Certificate=true";
-
-    builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseNpgsql(connectionString));
 }
+else
+{
+    // Conexión local desde appsettings.json
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+
+// Registrar DbContext con PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
+// Middleware
 app.UseStaticFiles();
 app.UseRouting();
+
 app.MapRazorPages();
 
+// Aplicar migraciones automáticamente
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
