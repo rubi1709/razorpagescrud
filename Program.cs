22c80b8cbae6d077292ad7e98bdcac4b7f4ff68c
Felicitas Rubi Diego Garcia <23300084@uttt.edu.mx>
2026-03-12 10:05:42 -0600

    enable migrations

--- Program.cs
+++ Program.cs
@@ -36,5 +36,10 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapRazorPages();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
